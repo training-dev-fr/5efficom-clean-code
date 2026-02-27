b9d0e35af8569f08d5d5a791fa5d8dc35e17ac9d
training-dev <contact@training-dev.fr>
2026-02-27 16:39:50 +0100

    code

---
+++ User.php
@@ -0,0 +1,44 @@
+<?php
+class User 
+{  
+    private $firstName; 
+    private $lastName; 
+    private static $regex;
+
+    public function __construct()
+    {
+        self::$regex = "/[a-zA-Zéèêîûô]{2,50}/U";
+    }
+
+    public function authenticate($email, $password) 
+    {   
+        if($email !== "avaast" || $password !== "123456")
+        {
+            return false;
+        }
+        return true;
+    }
+
+
+
+    public function updateUser($db)
+    {
+        $db->save($this);
+    }
+    
+    public function setFirstName($newFirstName)
+    {
+        return $this->firstName = $newFirstName;
+    } 
+
+        public function getFullName()
+    {
+        return "$this->firstName $this->lastName";
+    } 
+    
+    private function checkNameFormat($name)
+    { 
+        return preg_match(self::$regex, $this->{$name}); 
+    }
+    
+}
---
+++ index.js
@@ -0,0 +1,22 @@
+/**
+ * class repretening a user
+ * @class User
+ */
+class User{
+    /**
+     * 
+     * @param {Object} user 
+     */
+    constructor(user){
+        this.firstname = user.firstname;
+        this.email = user.email;
+    }
+    /**
+     * 
+     */
+    getUserName(){
+
+    }
+}
+
+let user = new User();
